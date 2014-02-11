8e5cade9a46170733f3f7e53e3ed744f28d6e4c6
Zhe CAI <caizhe2011@gmail.com>
2014-02-11 12:39:30 -0500

    structure of projects.

---
+++ Common/src/linked_list_cycle_ii/Solution.java
@@ -0,0 +1,49 @@
+package linked_list_cycle_ii;
+
+/**
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
+ * http://oj.leetcode.com/problems/linked-list-cycle-ii/
+ * 
+ * Solution: Use fast & slow pointers.
+ * Fast moves two times faster than the slow.
+ * 2*(l+x) = l+x+c => l = c-x
+ * When meet, slow start from head again, and each move one step at one time.
+ * They will meet at loop start.
+ * 
+ * Complexity: Time - O(n), Space - O(1)
+ * 
+ * Attention: Before meet, fast has to move two steps before judge.
+ * Even if meet when after one step, discard; otherwise, won't get loop start.
+ * 
+ * @author CAI Zhe
+ */
+public class Solution {
+  public ListNode detectCycle(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+
+    // fast & slow pointers to test if there's loop
+    // If head is counted as one step does not matter. But be consistent.
+    ListNode slow = head;
+    ListNode fast = head;
+    do {
+      slow = slow.next;
+      if (fast.next != null) {
+        fast = fast.next.next;
+      }
+      if (fast == null || fast.next == null) {
+        return null;
+      }
+    } while (slow != fast);
+
+    // track loop start
+    slow = head;
+    while (slow != fast) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    return slow;
+  }
+}
