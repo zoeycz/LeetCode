431d2ab66a18a332b1cf1b9b5241850fc1777491
Zhe CAI <caizhe2011@gmail.com>
2014-02-11 14:36:03 -0500

    Generate Parentheses added.

---
+++ LeetCode/src/generate_parentheses/Solution.java
@@ -0,0 +1,46 @@
+package generate_parentheses;
+
+import java.util.ArrayList;
+
+/**
+ * Generate Parentheses
+ * http://oj.leetcode.com/problems/generate-parentheses/
+ * 
+ * Given n pairs of parentheses, write a function to generate all combinations
+ * of well-formed parentheses.
+ * 
+ * Solution: Recursively add one char at a time.
+ * Keep track of the total left and right parentheses to ensure correctness.
+ * 
+ * Complexity: Time - O(2^n), Space - O(n)
+ * 
+ * @author CAI Zhe
+ */
+public class Solution {
+  private ArrayList<String> res;
+
+  public ArrayList<String> generateParenthesis(int n) {
+    res = new ArrayList<String>();
+    if (n < 0)
+      return res;
+    if (n == 0) {
+      res.add("");
+      return res;
+    }
+
+    generateHelper("", 0, 0, n);
+    return res;
+  }
+
+  private void generateHelper(String s, int l, int r, int n) {
+    if (r == n) {
+      res.add(s);
+    }
+    if (l < n) {
+      generateHelper(s + "(", l + 1, r, n);
+    }
+    if (r < l) {
+      generateHelper(s + ")", l, r + 1, n);
+    }
+  }
+}
