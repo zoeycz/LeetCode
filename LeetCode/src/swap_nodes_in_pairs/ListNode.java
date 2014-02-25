a1dc50444944fc98be5bd53fd58b13a042442b3d
Zhe CAI <caizhe2011@gmail.com>
2014-02-25 14:46:22 -0500

    Swap Nodes in Pairs added.

---
+++ LeetCode/src/swap_nodes_in_pairs/ListNode.java
@@ -0,0 +1,11 @@
+package swap_nodes_in_pairs;
+
+public class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int val) {
+    this.val = val;
+    next = null;
+  }
+}
---
+++ LeetCode/src/swap_nodes_in_pairs/Solution.java
@@ -0,0 +1,44 @@
+package swap_nodes_in_pairs;
+
+/**
+ * Swap Nodes in Pairs
+ * http://oj.leetcode.com/problems/swap-nodes-in-pairs/
+ * 
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * 
+ * Solution: Use a helper function to swap the neighbor nodes and return leading
+ * one in result.
+ * Main function keeps track of loop and link to new pairs.
+ * 
+ * Complexity: Time - O(n), Space - O(1)
+ * 
+ * @author CAI Zhe
+ */
+public class Solution {
+  public ListNode swapPairs(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    head = swapPairsHelper(head);
+    ListNode pred = head.next;
+    ListNode current = pred.next;
+    while (current != null) {
+      pred.next = swapPairsHelper(current);
+      pred = current;
+      if (current.next == null)
+        break;
+      current = current.next;
+    }
+    return head;
+  }
+
+  private ListNode swapPairsHelper(ListNode first) {
+    if (first.next == null) {
+      return first;
+    }
+    ListNode second = first.next;
+    first.next = second.next;
+    second.next = first;
+    return second;
+  }
+}
