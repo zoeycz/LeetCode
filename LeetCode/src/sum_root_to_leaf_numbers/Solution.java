5b33a63cbcff7bb36c1a15eda601250384b115ad
Zhe CAI <caizhe2011@gmail.com>
2014-03-02 11:47:37 -0500

    Sum Root to Leaf Numbers added.

--- LeetCode/src/sum_root_to_leaf_numbers/Solution.java
+++ LeetCode/src/sum_root_to_leaf_numbers/Solution.java
@@ -1,29 +1,41 @@
 package sum_root_to_leaf_numbers;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import auxiliary_data_structure.TreeNode;
 
+/**
+ * Sum Root to Leaf Numbers
+ * http://oj.leetcode.com/problems/sum-root-to-leaf-numbers/
+ * 
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+ * could represent a number.
+ * An example is the root-to-leaf path 1->2->3 which represents the number 123.
+ * Find the total sum of all root-to-leaf numbers.
+ * 
+ * Solution: DFS. Parse current node and number as parameter.
+ * When encounter a leaf, add to sum.
+ * 
+ * Complexity: Time - O(n), Space - O(1)
+ * 
+ * @author CAI Zhe
+ */
 public class Solution {
-  private List<Integer> list = new LinkedList<Integer>();
+  private int sum;
 
   public int sumNumbers(TreeNode root) {
     if (root == null) {
       return 0;
     }
+
+    sum = 0;
     traversal(root, 0);
-    int sum = 0;
-    for (int x : list) {
-      sum += x;
-    }
+
     return sum;
   }
 
   private void traversal(TreeNode root, int num) {
     num = num * 10 + root.val;
     if (root.left == null && root.right == null) {
-      list.add(num);
+      sum += num;
     } else {
       if (root.left != null) {
         traversal(root.left, num);
