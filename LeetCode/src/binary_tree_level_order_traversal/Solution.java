5e9dc1da6f4321dd897898f4547b5fafd09c2916
Zhe CAI <caizhe2011@gmail.com>
2014-03-02 11:30:15 -0500

    Binary Tree Level Order Traversal added.

---
+++ LeetCode/src/binary_tree_level_order_traversal/Solution.java
@@ -0,0 +1,70 @@
+package binary_tree_level_order_traversal;
+
+import java.util.ArrayList;
+
+/**
+ * Binary Tree Level Order Traversal
+ * http://oj.leetcode.com/problems/binary-tree-level-order-traversal/
+ * 
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * 
+ * Solution: BFS tree into an ArrayList, use null to separate two level.
+ * Keep count of consecutive nulls to detection traversal end.
+ * 
+ * Optimization: Helper tree can be used directly to get the final result to
+ * improve extra space usage from O(n) to O(1).
+ * 
+ * Complexity: Time - O(n), Space - O(n)
+ * 
+ * @author CAI Zhe
+ */
+public class Solution {
+  private ArrayList<TreeNode> tree;
+
+  public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    if (root == null) {
+      return result;
+    }
+
+    tree = new ArrayList<TreeNode>();
+    levelTree(root);
+
+    ArrayList<Integer> level = new ArrayList<Integer>();
+    for (TreeNode node : tree) {
+      if (node != null) {
+        level.add(node.val);
+      } else {
+        result.add(level);
+        level = new ArrayList<Integer>();
+      }
+    }
+    return result;
+  }
+
+  private void levelTree(TreeNode root) {
+    tree.add(root);
+    tree.add(null);
+    TreeNode current = root;
+    TreeNode last = root;
+    for (int i = 0;; i++) {
+      current = tree.get(i);
+      if (last == null && current == null) {
+        break;
+      }
+      if (current == null) {
+        tree.add(null);
+      } else {
+        if (current.left != null) {
+          tree.add(current.left);
+        }
+        if (current.right != null) {
+          tree.add(current.right);
+        }
+      }
+      last = current;
+    }
+    tree.remove(tree.size() - 1);
+  }
+}
---
+++ LeetCode/src/binary_tree_level_order_traversal/TreeNode.java
@@ -0,0 +1,10 @@
+package binary_tree_level_order_traversal;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int val) {
+    this.val = val;
+  }
+}
