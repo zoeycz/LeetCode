35962a77661b617c4718984d623708163a1734ae
Zhe CAI <caizhe2011@gmail.com>
2014-03-15 20:25:23 -0400

    Combination Sum optimized.

--- LeetCode/src/combination_sum/Solution.java
+++ LeetCode/src/combination_sum/Solution.java
@@ -8,6 +8,7 @@ import java.util.Set;
 /**
  * Combination Sum
  * http://oj.leetcode.com/problems/combination-sum/
+ * 
  * Given a set of candidate numbers (C) and a target number (T), find all unique
  * combinations in C where the candidate numbers sums to T.
  * The same repeated number may be chosen from C unlimited number of times.
@@ -23,26 +24,23 @@ import java.util.Set;
  * Optimization: Use a helper function with end indicates largest element to
  * use. Larger elements may be decomposed into smaller elements in later
  * iteration, but won't be used again to generate duplicates.
+ * No duplicates would be generated as all recursive calls follow such rules.
  * 
  * Complexity: Better than DP solution in both time and space.
- * 
  * @author CAI Zhe
  */
 public class Solution {
   public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates,
       int target) {
-    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
     Arrays.sort(candidates);
-    Set<ArrayList<Integer>> set = getCombination(candidates,
+    ArrayList<ArrayList<Integer>> res = getCombination(candidates,
         candidates.length - 1, target);
-    if (set.size() == 0) {
-      return result;
-    }
-    return new ArrayList<ArrayList<Integer>>(set);
+    return res;
   }
 
-  Set<ArrayList<Integer>> getCombination(int[] candidates, int end, int target) {
-    Set<ArrayList<Integer>> result = new HashSet<ArrayList<Integer>>();
+  ArrayList<ArrayList<Integer>> getCombination(int[] candidates, int end,
+      int target) {
+    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
     ArrayList<Integer> list;
     for (int i = end; i >= 0; i--) {
       if (candidates[i] == target) {
@@ -50,7 +48,7 @@ public class Solution {
         list.add(candidates[i]);
         result.add(list);
       } else if (candidates[i] < target) {
-        Set<ArrayList<Integer>> set;
+        ArrayList<ArrayList<Integer>> set;
         set = getCombination(candidates, i, target - candidates[i]);
         if (set.size() > 0) {
           for (ArrayList<Integer> inSet : set) {
